d51946a1638590d144949071f06a4934b2c9b7a3
Joseph Aidoo <kojoaidoi@gmail.com>
2024-05-31 05:01:31 +0000

    ucc

--- src/UserService.js
+++ src/UserService.js
@@ -121,7 +121,7 @@ class UserService{
       });
       return response.data;
     } catch (error) {
-      console.error('Error in forgotPassword:', error); 
+      // console.error('Error in forgotPassword:', error); 
       throw error;
     }
   }
