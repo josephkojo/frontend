b4c57960778b2c14abccf46b4e180f51403b9991
Joseph Aidoo <kojoaidoi@gmail.com>
2024-06-03 18:02:25 +0000

    changes in the login pages

--- src/Pages/Nav.jsx
+++ src/Pages/Nav.jsx
@@ -11,7 +11,7 @@ const Nav = () => {
   const handleCloseLogoutModal = () => setShowLogoutModal(false);
 
   const handleLogout = () => {
-    UserService.logout();
+    // UserService.logout();
     navigate('/');
   };
 
